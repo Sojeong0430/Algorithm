163140427d85aef16140adc8f460bab720d94a11
Sojeong0430 <162033491+Sojeong0430@users.noreply.github.com>
2024-04-17 00:42:20 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+    center = int(len(s)/2)
+    
+    if len(s)%2==0:
+        answer = s[center-1]+s[center]
+    else:
+        answer = s[center]
+        
+    return answer
