2d53ced913300c7834fa35604057b9843d61550d
Sojeong0430 <162033491+Sojeong0430@users.noreply.github.com>
2024-04-17 12:34:58 +0900

    [Bronze II] Title: 저항, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,8 @@
+co1 = input()
+co2 = input()
+co3 = input()
+
+
+color = {'black':0,'brown':1,'red':2,'orange':3,'yellow':4,'green':5,'blue':6,'violet':7,'grey':8,'white':9}
+
+print((color[co1]*10+color[co2])*(10**color[co3]))
