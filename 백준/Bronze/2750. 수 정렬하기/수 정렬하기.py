6a4c1dae2334c2ac81254bfd65b3fc47867d089a
Sojeong0430 <162033491+Sojeong0430@users.noreply.github.com>
2024-07-08 01:50:35 +0900

    [Bronze II] Title: 수 정렬하기, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,13 +1,5 @@
 count = int(input())
-list=[]
-for _ in range (count):
-    num = int(input())
-    list.append(num)
-
-while len(list)!=0:
-    min = list[0]
-    for i in range(len(list)):
-        if min > list[i]:
-            min = list[i]
-    print(min)
-    list.remove(min)
+nums = [int(input()) for i in range(count)]
+nums = sorted(nums)
+for i in range(count):
+    print(nums[i])
