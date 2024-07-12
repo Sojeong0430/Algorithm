72b06113f269ee7e72de23d55b1f19998c777023
Sojeong0430 <162033491+Sojeong0430@users.noreply.github.com>
2024-07-12 23:21:17 +0900

    [Bronze I] Title: 최소공배수, Time: 36 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -8,7 +8,4 @@ for _ in range(t):
     numlist.append(list(map(int,sys.stdin.readline().split())))
 
 for i in numlist:
-    a,b=i[0],i[1]
-    gcd = math.gcd(a,b)
-    result = abs(a*b)//gcd
-    print(result)
+    print(math.lcm(i[0],i[1]))
