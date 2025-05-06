eccde30e3948891efe3cc40507bf56781b4b7b86
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-05-06 19:55:32 +0900

    [Bronze II] Title: 문자열 반복, Time: 196 ms, Memory: 18592 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,20 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int t = sc.nextInt();
+        sc.nextLine();
+        String[] result = new String[t];
 
-        for (int i = 0 ; i < n ; i++){
-            int r = sc.nextInt();
-            String s = sc.next();
-            String result = "";
-            for(int k = 0 ; k < s.length() ; k++){
-                for(int j = 0 ; j < r ; j++){
-                    result += s.charAt(k);
+        for (int i = 0; i < t; i++) {
+            String temp_result = "";
+            String temp = sc.nextLine();
+            String[] arr = temp.split(" ");
+            for(int j = 0 ; j < arr[1].length() ; j++){
+                for(int k = 0 ; k < Integer.parseInt(arr[0]) ; k++){
+                    temp_result += arr[1].charAt(j);
                 }
             }
-            System.out.println(result);
+            result[i] = temp_result;
+        }
+
+        for(int i = 0 ; i < t ; i++){
+            System.out.println(result[i]);
         }
     }
 }
