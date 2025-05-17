ddb177c903657a09715c3f3e6f4be5b8667e684f
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-05-18 00:18:23 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1336 ms, Memory: 187180 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,31 +1,32 @@
-import java.util.*;
-
+import java.util.Scanner;
+import java.util.Arrays;
+ 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        int[][] arr = new int[n][2];
-
-        for(int i = 0; i < n; i++) {
-            int[] temp =  Arrays.stream(sc.nextLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-
-            arr[i][0] = temp[0];
-            arr[i][1] = temp[1];
-        }
-
-        Arrays.sort(arr, (a,b) -> { 
-            if(a[0] != b[0]){ // 첫 번째 기준
-                return Integer.compare(a[0], b[0]);
-            }else{ // 두 번째 기준
-                return Integer.compare(a[1], b[1]);
-            }
-        });
-
-        for(int[] m : arr){
-            System.out.println(m[0] + " " + m[1]);
-        }
-    }
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		int[][] arr = new int[N][2];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i][0] = in.nextInt();
+			arr[i][1] = in.nextInt();
+		}
+		
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
