ea1213a8d2af84a839b84b11f1d3b6efb438dc2d
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-11-11 18:01:23 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,45 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.min;
 
 public class Main {
-    static int[][] houses;
-    static int[][] cost;
-    static int N;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(br.readLine());
+        int[][] price = new int[n][3];
+        int[][] dp = new int[n][3]; // 1:red 2:green 3:blue
 
-        N = Integer.parseInt(st.nextToken());
-        houses = new int[N][3];
-        cost = new int[N][3];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < 3; j++) {
-                houses[i][j] = Integer.parseInt(st.nextToken());
-            }
+        for(int i = 0 ; i < n ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            price[i][0] = Integer.parseInt(st.nextToken()); // red
+            price[i][1] = Integer.parseInt(st.nextToken()); // green
+            price[i][2] = Integer.parseInt(st.nextToken()); // blue
         }
 
-        cost[0][0] = houses[0][0];
-        cost[0][1] = houses[0][1];
-        cost[0][2] = houses[0][2];
+        dp[0][0] = price[0][0];
+        dp[0][1] = price[0][1];
+        dp[0][2] = price[0][2];
 
-        System.out.println(Math.min(Math.min(dp(N-1,0), dp(N-1,1)), dp(N-1,2)));
-    }
-
-    public static int dp(int N, int color) {
-        if (cost[N][color] == 0) {
-            if (color == 0) {
-                return cost[N][color] = Math.min(dp(N-1, 1), dp(N-1, 2)) + houses[N][color];
-            }
-            if (color == 1) {
-                return cost[N][color] = Math.min(dp(N-1, 0), dp(N-1, 2)) + houses[N][color];
-            }
-            if (color == 2) {
-                return cost[N][color] = Math.min(dp(N-1, 0), dp(N-1, 1)) + houses[N][color];
-            }
+        for(int i = 1 ; i < n ; i++){
+            dp[i][0] = price[i][0] + min(dp[i-1][1], dp[i-1][2]);
+            dp[i][1] = price[i][1] + min(dp[i-1][0], dp[i-1][2]);
+            dp[i][2] = price[i][2] + min(dp[i-1][1], dp[i-1][0]);
         }
-        return cost[N][color];
+
+        int answer = min(dp[n-1][0], min(dp[n-1][1], dp[n-1][2]));
+        System.out.println(answer);
     }
 }
