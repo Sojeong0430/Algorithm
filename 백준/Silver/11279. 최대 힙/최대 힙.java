a6a18eccbbec873c2daae389ec33f1a7151027a4
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-01-07 20:40:33 +0900

    [Silver II] Title: 최대 힙, Time: 780 ms, Memory: 31852 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0; i < n; i++) {
+            int a = Integer.parseInt(br.readLine());
+            if(a == 0){
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            }else{
+                pq.offer(a);
+            }
+        }
+    }
+}
