d006d80a6ad9905f6d0b3cecaab0d5c9c9b62264
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-01-13 16:11:49 +0900

    [Bronze II] Title: 소수, Time: 96 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,53 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+
 public class Main {
- 
-	public static boolean prime[];
-	
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int M = Integer.parseInt(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-		
-		prime = new boolean[N + 1];
-		get_prime();
-		
-		
-		int sum = 0;
-		int min = Integer.MAX_VALUE;
-		for(int i = M; i <= N; i++) {
-			if(prime[i] == false) {	 
-				sum += i;
-				if(min == Integer.MAX_VALUE) {
-					min = i;
-				}
-			}
-		}
-		
-		if(sum == 0) {
-			System.out.println(-1);
-		}
-		else {
-			System.out.println(sum);
-			System.out.println(min);
-		}
-		
-	}
- 
-	 
-	public static void get_prime() {
-		prime[0] = true;
-		prime[1] = true;
-		
-		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-			if(prime[i]) continue;	
-			for(int j = i * i; j < prime.length; j += i) {
-				prime[j] = true;
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        boolean exist = false;
+
+        for(int i = m ; i <= n ; i++){
+            boolean isPrime = true;
+            if(i == 1){
+                continue;
+            }
+            for(int j = 2; j * j <= i; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                exist = true;
+                sum += i;
+                if(i < min){
+                    min = i;
+                }
+            }
+        }
+
+        if(exist){
+            System.out.println(sum);
+            System.out.print(min);
+        }
+        else{
+           System.out.print(-1);
+        }
+    }
 }
