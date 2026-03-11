5fdad0a6da6e0ad03e3cd67e74335cbea9a9924b
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-03-12 03:49:31 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        if(n <= 2){
+            System.out.print(n);
+            return;
+        }
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n ; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+
+        System.out.print(dp[n]);
+    }
+}
