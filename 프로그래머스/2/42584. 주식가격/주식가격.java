3e6ab0c54b8d16aa8ccdf17eea5523e999a4e8c9
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-07-31 01:49:56 +0900

    [level 2] Title: 주식가격, Time: 22.00 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0 ; i < prices.length ; i++){
+            
+            while(!stack.isEmpty() && 
+                  prices[stack.peek()] > prices[i]){
+                
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            
+            stack.push(i);
+        }
+        
+        while(!stack.isEmpty()){
+            int idx = stack.pop();
+            answer[idx] = prices.length - 1 - idx;
+        }
+        
+        return answer;
+    }
+}
