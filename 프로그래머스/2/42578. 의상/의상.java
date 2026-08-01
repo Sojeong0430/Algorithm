5b9ce1879980643ab2ba7ec0ba2bc9cacfaa4225
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-08-02 01:05:00 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String[] cloth : clothes){
+            map.put(cloth[1], map.getOrDefault(cloth[1],0) + 1);
+        }
+        
+        int count = 1;
+        for(String cloth : map.keySet()){
+            count *= map.get(cloth)+1;
+        }
+        
+        return count - 1;
+    }
+}
