4fb7afad4c89463650076fed3685075d73709701
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-08-10 16:12:53 +0900

    [level 2] Title: 롤케이크 자르기, Time: 129.40 ms, Memory: 205 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        
+        int count = 0;
+        
+        for(int i = 0 ; i < topping.length ; i++){
+            map.put(topping[i], map.getOrDefault(topping[i], 0)+1);
+        }
+        
+        for(int i = 0 ; i < topping.length ; i++){
+            set.add(topping[i]);
+            map.put(topping[i], map.get(topping[i])-1);
+            
+            if(map.get(topping[i]) == 0){
+                map.remove(topping[i]);
+            }
+            
+            if(set.size() == map.size()){
+                count ++;
+            }
+        }
+        
+        return count;
+    }
+}
