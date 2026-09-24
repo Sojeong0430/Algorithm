1967b6c7c2887b364c5ea6acaf69aa102e96b25a
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-09-25 04:38:16 +0900

    [level 2] Title: 구명보트, Time: 9.76 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        int answer = 0;
+        
+        while(left <= right){
+            if(people[left] + people[right] <= limit){
+                left += 1;
+            }
+            
+            right -= 1;
+            answer += 1;
+        }
+        
+        return answer;
+    }
+}
